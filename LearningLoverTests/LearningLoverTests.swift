f5e2b2fc4a3f9fb373019f1ace3a9c05c337caa5
sinhlh <lehoangsinhhn99@gmail.com>
2023-12-29 16:57:41 +0700

    create the first acceptant test

--- LearningLoverTests/LearningLoverTests.swift
+++ LearningLoverTests/LearningLoverTests.swift
@@ -6,31 +6,56 @@
 //
 
 import XCTest
+import ViewInspector
 @testable import LearningLover
+
+struct TransactionModel {
+    
+}
+
+import SwiftUI
+struct TransactionListView: View {
+    var body: some View {
+        Text("")
+    }
+}
 
 final class LearningLoverTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_onAppLaunch_displaysEmptyTransactionlist() throws {
+        let newTransaction = TransactionModel()
+        let view = try launch()
+        
+        view.simulateAdding(transaction: newTransaction)
+        
+        XCTAssertEqual(view.list().count, 0)
+    }
+    
+    private func launch() throws -> TransactionListView {
+        let view = TransactionListView()
+        
+        return view
+    }
+    
+    class TestApp {
+        
+        private func selectAddTransaction() { }
+        
+        private func selectAmount(for transaction: TransactionModel) {}
+        private func setCategory(for transaction: TransactionModel) {}
+        private func save(_ transaction: TransactionModel) {}
+        
+        
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+}
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+extension TransactionListView {
+    func list() -> [TransactionModel] {
+        return []
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func simulateAdding(transaction: TransactionModel) {
+        
     }
-
 }
