16674476b97a2fa2e8984538af8a2c4ea3bf3d87
sinhlh <lehoangsinhhn99@gmail.com>
2023-12-29 16:14:39 +0700

    Initial Commit

---
+++ LearningLover/LearningLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningLoverApp.swift
+//  LearningLover
+//
+//  Created by SinhLH.FHN on 29/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
