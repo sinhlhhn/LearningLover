a12b9fe6ce769b7ef709a4cdbf8174133bd9dfc8
sinhlh <lehoangsinhhn99@gmail.com>
2024-01-03 13:23:09 +0700

    refactor code to remove duplication

--- LHSCore/LHSCoreTests/RemoteCategoryLoaderTest.swift
+++ LHSCore/LHSCoreTests/RemoteCategoryLoaderTest.swift
@@ -21,32 +21,37 @@ protocol HTTPClient {
     func get(from url: URL)
 }
 
-class HTTPCLientSpy: HTTPClient {
-    var requestedURL: URL?
-    
-    func get(from url: URL) {
-        self.requestedURL = url
-    }
-}
-
 final class RemoteCategoryLoaderTest: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let url = URL(string: "http://a-given-url")!
-        let client = HTTPCLientSpy()
-        _ = RemoteCategoryLoader(url: url, client: client)
+        let (_, client) = makeSUT()
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
         let url = URL(string: "http://a-given-url")!
-        let client = HTTPCLientSpy()
-        let sut = RemoteCategoryLoader(url: url, client: client)
+        let (sut, client) = makeSUT(url: url)
         
         sut.load()
         
         XCTAssertEqual(client.requestedURL, url)
     }
 
+    //MARK: - Helpers:
+    
+    private func makeSUT(url: URL = URL(string: "http://a-default-url")!) -> (RemoteCategoryLoader, HTTPCLientSpy) {
+        let client = HTTPCLientSpy()
+        let sut = RemoteCategoryLoader(url: url, client: client)
+        
+        return (sut, client)
+    }
+    
+    private class HTTPCLientSpy: HTTPClient {
+        var requestedURL: URL?
+        
+        func get(from url: URL) {
+            self.requestedURL = url
+        }
+    }
 }
