a1e491555b628124f994954a3d93db933b08a5c6
sinhlh <lehoangsinhhn99@gmail.com>
2024-01-03 10:22:04 +0700

    test init displays empty list

--- LHSCore/LHSCoreTests/LHSCoreTests.swift
+++ LHSCore/LHSCoreTests/LHSCoreTests.swift
@@ -1,12 +1,22 @@
 //
 
 import XCTest
-@testable import LHSCore
+import LHSCore
+
+class MemoryStore {
+    func load() async throws -> [String] {
+        return []
+    }
+}
 
 class LHSCoreTests: XCTestCase {
 
-    func test_init() {
-        XCTFail()
+    func test_init_displaysEmptyList() async throws {
+        let sut = MemoryStore()
+        
+        let result = try await sut.load()
+        
+        XCTAssertEqual(result.count, 0)
     }
 
 }
