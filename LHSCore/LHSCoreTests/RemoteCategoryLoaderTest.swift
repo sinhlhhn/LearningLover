0d856a203330bbaa62407dcec1ac0c20af6b46e0
sinhlh <lehoangsinhhn99@gmail.com>
2024-01-03 11:30:29 +0700

    `RemoteCategoryLoader` load data from URL

--- LHSCore/LHSCoreTests/RemoteCategoryLoaderTest.swift
+++ LHSCore/LHSCoreTests/RemoteCategoryLoaderTest.swift
@@ -1,22 +1,48 @@
 //
 
 import XCTest
+import Combine
 
 class RemoteCategoryLoader {
+    let client: HTTPClient
     
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func load() {
+        client.get(from: URL(string: "http://a-url.com")!)
+    }
 }
 
-class HTTPClient {
+protocol HTTPClient {
+    func get(from url: URL)
+}
+
+class HTTPCLientSpy: HTTPClient {
     var requestedURL: URL?
+    
+    func get(from url: URL) {
+        self.requestedURL = url
+    }
 }
 
 final class RemoteCategoryLoaderTest: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
-        _ = RemoteCategoryLoader()
+        let client = HTTPCLientSpy()
+        _ = RemoteCategoryLoader(client: client)
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPCLientSpy()
+        let sut = RemoteCategoryLoader(client: client)
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 
 }
