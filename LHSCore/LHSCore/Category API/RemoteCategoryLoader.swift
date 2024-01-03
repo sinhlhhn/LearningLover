7c2fa7ab863f83be4cbc41cf372d2eb3e836befc
sinhlh <lehoangsinhhn99@gmail.com>
2024-01-03 13:34:07 +0700

    move `CategoryLoader` to production

---
+++ LHSCore/LHSCore/Category API/RemoteCategoryLoader.swift
@@ -0,0 +1,21 @@
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public final class RemoteCategoryLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
--- LHSCore/LHSCoreTests/RemoteCategoryLoaderTest.swift
+++ LHSCore/LHSCoreTests/RemoteCategoryLoaderTest.swift
@@ -1,25 +1,7 @@
 //
 
 import XCTest
-import Combine
-
-class RemoteCategoryLoader {
-    let url: URL
-    let client: HTTPClient
-    
-    init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-    
-    func load() {
-        client.get(from: url)
-    }
-}
-
-protocol HTTPClient {
-    func get(from url: URL)
-}
+import LHSCore
 
 final class RemoteCategoryLoaderTest: XCTestCase {
 
